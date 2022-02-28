356ae711e1c07f216ec2b4767e7f55c381fe447c
Sanggi Min <samin@student.42seoul.kr>
2022-02-28 20:45:20 +0900

    003

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React  from "react";
 import './App.css'
+import ImportConponent from './R003_ImportConponent'
+
 function App()
 {
     return (
         <div>
           <h1>Start React 200!</h1>
-          <p>HTML 적용하기</p>
+          <p>CSS 적용하기</p>
+          <ImportConponent></ImportConponent>
         </div>
     );
 }
